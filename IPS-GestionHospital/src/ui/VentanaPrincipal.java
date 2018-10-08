9788e3cc98375d8f23bf3c1602d71aa8f1a6f048
Vic <victor.conil.14@gmail.com>
2018-10-09 00:27:07 +0200

    Commit de prueba

--- IPS-GestionHospital/src/ui/VentanaPrincipal.java
+++ IPS-GestionHospital/src/ui/VentanaPrincipal.java
@@ -4,12 +4,20 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+	
 	private JPanel contentPane;
+	private JButton btnMenDeCitas;
 
 	/**
 	 * Launch the application.
@@ -18,8 +26,10 @@ public class VentanaPrincipal extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VentanaPrincipal frame = new VentanaPrincipal();
-					frame.setVisible(true);
+					VentanaPrincipal vP = new VentanaPrincipal();
+					vP.setLocationRelativeTo(null);
+					vP.setTitle("Menu Principal...");
+					vP.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -28,15 +38,32 @@ public class VentanaPrincipal extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Constructor de la ventana.
 	 */
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(3, 0, 0, 0));
+		contentPane.add(getBtnMenDeCitas());
 	}
 
+	private JButton getBtnMenDeCitas() {
+		if (btnMenDeCitas == null) {
+			btnMenDeCitas = new JButton("Men\u00FA de citas");
+			btnMenDeCitas.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					//Aquí se llamaría a la pantalla de citas
+					mostrarMensaje("La ventana aun no ha sido implementada", "SECCIÓN NO IMPLEMENTADA", JOptionPane.WARNING_MESSAGE);
+				}
+			});
+		}
+		return btnMenDeCitas;
+	}
+	
+	private void mostrarMensaje(String mess, String title, int icon) {
+		JOptionPane.showMessageDialog(this, mess, title, icon);
+	}
 }
