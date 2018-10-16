e7e980119a18643d8475888ff0bcc8ace6348e58
Vic <victor.conil.14@gmail.com>
2018-10-16 17:34:05 +0200

    Enlace Administardor--->ventanaFijarCita y arreglo en el DATACITAS

--- IPS-GestionHospital/src/ui/admin/VentanaFijarCita.java
+++ IPS-GestionHospital/src/ui/admin/VentanaFijarCita.java
@@ -85,22 +85,6 @@ public class VentanaFijarCita extends JFrame {
 	private EmpleadoDto paciente;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaFijarCita frame = new VentanaFijarCita();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public VentanaFijarCita() {
